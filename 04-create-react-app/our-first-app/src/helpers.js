2e58407bb5266640d747c24ab1c43c90d8debd2f
Ahad Hossain <hossain1814@gmail.com>
2020-10-11 17:40:07 +0600

    export and import modules in js ES6

---
+++ 04-create-react-app/our-first-app/src/helpers.js
@@ -0,0 +1,15 @@
+function helpful() {
+  console.log('I did a super helpful thing');
+};
+
+function sort() {
+  console.log('All Sorted');
+}
+
+function sing() {
+  console.log('La La La');
+}
+
+export default helpful;
+
+export {sing, sort};
