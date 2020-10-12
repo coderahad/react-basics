566b734dea99c1e230f656f819e4bbdfe1f69377
Ahad Hossain <hossain1814@gmail.com>
2020-10-12 14:17:25 +0600

    some convetions when create a react app. css, img, logo in react

---
+++ 04-create-react-app/our-first-app/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Dog from './Dog';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+        <Dog />
+        <div>I AM ANOTHER DIV, NOT IN DOG COMPONENT</ div>
+        <img className="Dog-img" src={logo}/>
+			</div>
+		);
+	}
+}
+
+export default App;
--- 04-create-react-app/our-first-app/src/index.js
+++ 04-create-react-app/our-first-app/src/index.js
@@ -1,15 +1,12 @@
-import foods from './foods';
-import { choice, remove } from './helpers';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-// Randomly draw a fruit from the array
-let fruit = choice(foods);
-// Log the message “I’d like one RANDOMFRUIT, please.”
-console.log(`I would like the ${fruit}, please`);
-// Log the message “Here you go: RANDOMFRUIT”
-console.log(`Here you go: ${fruit}`);
-// Log the message “Delicious! May I have another?”
-console.log(`Delicious! May I have another?`);
-// Remove the fruit from the array of fruits
-const fruitsLeft = remove(foods, fruit);
-// Log the message “I’m sorry, we’re all out. We have FRUITSLEFT left.”
-console.log(`I’m sorry, we’re all out. We have ${fruitsLeft} left.`);
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
