afb700e928ebe522167bb332ef5606b5d452fe4f
Ahad Hossain <hossain1814@gmail.com>
2020-10-13 01:11:03 +0600

    exercise five pokegame starts. Create a pokecard.

---
+++ 05-pokedex-project/pokegame/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+import Pokecard from './Pokecard';
+
+class App extends Component {
+  render() {
+    return(
+      <div className="App">
+        <Pokecard
+        id='4'
+        name='Charmander'
+        type='fire'
+        base_experience='62' 
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
