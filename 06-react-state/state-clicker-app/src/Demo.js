6f0ba8dc1dde80652e1606032b882a9ee4e42bbd
Ahad Hossain <hossain1814@gmail.com>
2020-10-17 13:10:02 +0600

    what super() is in constructor.

--- 06-react-state/state-clicker-app/src/Demo.js
+++ 06-react-state/state-clicker-app/src/Demo.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 
 class Demo extends Component {
   constructor(props) {
-    super();
+    super(props);
+    this.state = { color: 'cyan'};
     console.log(this.props);
   }
   render() {
     return(
       <div>
-        <h1>{this.props.animal}</h1>
+        <h1>{this.props.animal} {this.state.color}</h1>
       </div>
     )
   }
