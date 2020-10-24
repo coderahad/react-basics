5ebc2fd2ea388b7182556ef30881a80157e6bcb0
Ahad Hossain <hossain1814@gmail.com>
2020-10-24 19:03:09 +0600

    alternate syntax part 2.

--- 06-react-state/state-clicker-app/src/App.js
+++ 06-react-state/state-clicker-app/src/App.js
@@ -4,13 +4,14 @@ import Demo from './Demo';
 import Rando from './Rando';
 import Button from './Button';
 import BrokenClick from './BrokenClick';
+import BrokenClick2 from './BrokenClick2';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <BrokenClick /> 
+        <BrokenClick2 /> 
       </div>
     );
   }
