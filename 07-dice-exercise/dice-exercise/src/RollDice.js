9d77abfa96a260ff79b2db9ddc5478290740170b
Ahad Hossain <hossain1814@gmail.com>
2020-10-25 17:32:42 +0600

    third commit for dice.

--- 07-dice-exercise/dice-exercise/src/RollDice.js
+++ 07-dice-exercise/dice-exercise/src/RollDice.js
@@ -1,20 +1,30 @@
 import React, { Component } from 'react';
 import Die from './Die';
+import numberToWords from 'number-to-words';
+import './RollDice.css';
 
 class RollDie extends Component {
   static defaultProps = {
-    sides : ['one', 'tow', 'three', 'four', 'five', 'six']
+    sides : ['one', 'two', 'three', 'four', 'five', 'six']
   }
   constructor(props) {
     super(props);
-    this.state = { die1: "one", die2: "one" }
+    this.state = { die1: "one", die2: "one" };
+    this.roll = this.roll.bind(this);
   }
-  
+  roll() {
+    const rand1 = this.props.sides[Math.floor(Math.random() * this.props.sides.length)];
+    const rand2 = numberToWords.toWords(Math.floor(Math.random() * this.props.sides.length) + 1);
+    this.setState({ die1: rand1, die2: rand2 });
+  }
   render() { 
     return ( 
-      <div>
-        <Die face={this.state.die1}/>
-        <Die face={this.state.die2}/>
+      <div className='RollDice'>
+        <div className='RollDice-container'>
+          <Die face={this.state.die1}/>
+          <Die face={this.state.die2}/>
+        </div>
+        <button onClick={this.roll}>Roll Dice!</button>
       </div>
      );
   }
