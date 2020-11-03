0ab8ca9c03f871ce8b2e5ab7debfb56137bf370d
Ahad Hossain <hossain1814@gmail.com>
2020-11-03 14:03:44 +0600

    react events exercise, onMouseEnter or onMouseLeave

---
+++ 10. The World of React Events/events-exercises/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import WiseSquare from './WiseSquare';
+
+function App() {
+  return (
+    <div className="App">
+      <WiseSquare />
+    </div>
+  );
+}
+
+export default App;
