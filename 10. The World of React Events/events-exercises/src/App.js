54a7db5611fd8610bcf786d1ae173d43d8eda28c
Ahad Hossain <hossain1814@gmail.com>
2020-11-03 16:45:54 +0600

    onCopy event.

--- 10. The World of React Events/events-exercises/src/App.js
+++ 10. The World of React Events/events-exercises/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import WiseSquare from './WiseSquare';
 import AnnoyingForm from './AnnoyingForm';
+import CopyDemo from './CopyDemo';
 
 function App() {
   return (
     <div className="App">
       <WiseSquare />
       <AnnoyingForm />
+      <CopyDemo />
     </div>
   );
 }
