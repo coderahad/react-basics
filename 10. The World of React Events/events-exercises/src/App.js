6415acfa8e35b0844658aaeecc745569294d07ba
Ahad Hossain <hossain1814@gmail.com>
2020-11-19 12:22:25 +0600

    First commit for understanding 'this'. Here script can not read 'this'.

--- 10. The World of React Events/events-exercises/src/App.js
+++ 10. The World of React Events/events-exercises/src/App.js
@@ -3,13 +3,13 @@ import './App.css';
 import WiseSquare from './WiseSquare';
 import AnnoyingForm from './AnnoyingForm';
 import CopyDemo from './CopyDemo';
+import WiseSquareWithProps from './WiseSquareWithProps';
 
 function App() {
   return (
     <div className="App">
       <WiseSquare />
-      <AnnoyingForm />
-      <CopyDemo />
+      <WiseSquareWithProps />
     </div>
   );
 }
