91896ef49df1f6053c0ae3567f9cca0229aa107e
Ahad Hossain <hossain1814@gmail.com>
2020-11-21 07:43:02 +0600

    third commit.passing remove method with argument in child with the help of another function in child constructor.

--- 10. The World of React Events/events-exercises/src/BetterNumberItem.js
+++ 10. The World of React Events/events-exercises/src/BetterNumberItem.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 
 class BetterNumberItem extends Component {
-  
+  constructor(props) {
+    super(props);
+    this.handleRemove = this.handleRemove.bind(this);
+  }
+  handleRemove(evt) {
+    this.props.remove(this.props.value);
+  }
   render() { 
     return ( 
       <li>
         {this.props.value}
-        <button onClick={this.props.remove}>X</button>
+        <button onClick={this.handleRemove}>X</button>
       </li>
      );
   }
