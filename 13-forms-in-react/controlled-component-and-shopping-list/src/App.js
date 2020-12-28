2053be5dc50f5acc4444d0f382c5492bbf85d245
Ahad Hossain <hossain1814@gmail.com>
2020-12-28 14:12:05 +0600

    5th cmt. shopping list initial.

--- 13-forms-in-react/controlled-component-and-shopping-list/src/App.js
+++ 13-forms-in-react/controlled-component-and-shopping-list/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Form from './Form';
 import MultipleForm from './MultipleForm';
+import ShoppingList from './ShoppingList';
+import ShoppingListForm from './ShoppingListForm';
 
 function App() {
   return (
     <div className="App">
-      <Form />
-      <MultipleForm />
+      <ShoppingList />
+      {/* <ShoppingListForm /> */}
     </div>
   );
 }
