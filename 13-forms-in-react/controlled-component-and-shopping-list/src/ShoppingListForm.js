ca9774aa62322fb3f7932414b829d1829dd27fe2
Ahad Hossain <hossain1814@gmail.com>
2020-12-28 14:16:55 +0600

    6th cmt. shopping form manipulate.

--- 13-forms-in-react/controlled-component-and-shopping-list/src/ShoppingListForm.js
+++ 13-forms-in-react/controlled-component-and-shopping-list/src/ShoppingListForm.js
@@ -1,24 +1,24 @@
 import React, { Component } from "react";
 
 class ShoppingListForm extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = { name: '', qty: '' };
-  //   this.handleChange = this.handleChange.bind(this);
-  // }
-  // handleChange(evt) {
-  //   this.setState({
-  //     [evt.target.name]: evt.target.value
-  //   });
-  // }
+  constructor(props) {
+    super(props);
+    this.state = { name: '', qty: '' };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(evt) {
+    this.setState({
+      [evt.target.name]: evt.target.value
+    });
+  }
   render() {
     return (
-      // <form>
-      //   <lable htmlFor='name'>Name:</lable>
-      //   <input name='name' id='name' value={this.state.name} onChange={this.handleChange}/>
-      //   <lable htmlFor='qty'>Quantity:</lable>
-      //   <input name='qty' id='qty' onChange={this.handleChange}/>
-      // </form>
+      <form>
+        <lable htmlFor='name'>Name:</lable>
+        <input name='name' id='name' value={this.state.name} onChange={this.handleChange}/>
+        <lable htmlFor='qty'>Quantity:</lable>
+        <input name='qty' id='qty' onChange={this.handleChange}/>
+      </form>
   )}
 }
 export default ShoppingListForm;
